f2fabb97d3c2b5ab30abaf1158f089b1227875c3
Kamil Ogórek <kamil.ogorek@gmail.com>
2025-11-26 13:24:35 +0100

    inline css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import inlining from "vite-plugin-html-inline-sources";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), inlining()],
 });
