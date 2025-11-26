2c7dd758c1ca6bb190f01a45cec7421b006cc05c
Kamil Ogórek <kamil.ogorek@gmail.com>
2025-11-26 12:34:08 +0100

    dist -> docs for gh pages support

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  build: {
+    outDir: "docs",
+  },
   plugins: [tailwindcss()],
 });
